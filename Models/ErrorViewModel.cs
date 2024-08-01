095613b93e3f42bfe40b285bfad12d694be2eda6
jefersenac12 <jeferson.souza10@senacsp.edu.br>
2024-08-01 15:00:20 -0300

    subindo arquivos

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TI43_Teste.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
